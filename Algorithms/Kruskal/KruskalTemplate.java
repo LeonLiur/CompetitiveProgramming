57c7901abbeada3ad228035e45f58eb91c73c034
Leo Liu <leoliuhy@gmail.com>
2020-07-23 15:21:58 -0400

    Added KruskalTemplate.java

---
+++ Algorithms/Kruskal/KruskalTemplate.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class KruskalTemplate{
+    class Graph{
+        class Edge implements Comparable<Edge>{
+            int src, dest, weight;
+
+            public int compareTo(Edge comparedEdge){
+                return this.weight - comparedEdge.weight;
+            }
+
+        };
+
+        class subset{
+            int parent, rank;
+        }
+    }
+}
