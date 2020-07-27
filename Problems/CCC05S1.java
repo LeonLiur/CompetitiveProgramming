aa08f951b550aeb6768bfb1a6e5013ee7646672e
Leo Liu <leoliuhy@gmail.com>
2020-07-27 00:49:11 -0400

    Added File

---
+++ Problems/CCC05S1.java
@@ -0,0 +1,35 @@
+package Problems;
+
+import java.util.Scanner;
+
+public class CCC05S1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String input = sc.nextLine();
+            input = input.replace("-", "");
+            for (int j = 0; j < 10; j++) {
+
+                char x = input.toCharArray()[j];
+                if (x > 58 && x < 80) {
+                    input = input.replace(x, Integer.toString((x + 1) / 3 - 20).toCharArray()[0]);
+                } else if (x == 'Z') {
+                    input = input.replace('Z', '9');
+                } else if (x > 80){
+                    input = input.replace(x, Integer.toString((x)/3 - 20).toCharArray()[0]);
+                } else if (x == 'P'){
+                    input = input.replace('P', '7');
+                }
+
+                System.out.print(input.charAt(j));
+                if(j == 2 || j == 5){
+                    System.out.print('-');
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
