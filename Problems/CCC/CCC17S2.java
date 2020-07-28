2101bdef2868b427c20cfaf2fdb136f98b3a8779
Leo Liu <leoliuhy@gmail.com>
2020-07-28 15:12:19 -0400

    Added Solution for CCC

---
+++ Problems/CCC/CCC17S2.java
@@ -0,0 +1,34 @@
+package Problems.CCC;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CCC17S2 {
+
+    public static void main(String[]args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int[] arr = new int[a];
+        for(int i=0;i<a;i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        if(a%2!=0) {
+            Arrays.sort(arr);
+            for(int i=1;i<=a/2;i++) {
+                System.out.print(arr[(a/2)-i+1]+" ");
+                System.out.print(arr[(a/2)+i]+" ");
+            }
+            System.out.print(arr[0]);
+        }
+
+        if(a%2==0) {
+            Arrays.sort(arr);
+            for(int i=0;i<a/2;i++) {
+                System.out.print(arr[(a/2)-i-1]+" ");
+                System.out.print(arr[(a/2)+i]+" ");
+            }
+        }
+
+    }
+}
